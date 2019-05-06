cee2677f7fa395637c87b909636f3144c083b00e
Musashi <k.miyamoto.0128@gmail.com>
2019-05-07 02:10:30 +0900

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# react-graphql-search-apps-master
+## 手順
+# react app create
+npm install --global create-react-app
+
+# add allop & graphql
+yarn add apollo-boost@0.1.16 graphql@14.0.2 react-apollo@2.1.11
+
+
+# .envの設定
+echo REACT_APP_GITHUB_TOKEN= ["設定したTOKEN"] > .env.development.local
+
+# githubの設定
+Settings -> Personal access tokensで対応させるappの登録 && public_repoを許可
---
+++ src/graphql.js
@@ -0,0 +1,58 @@
+import gql from 'graphql-tag'
+
+export const ME = gql`
+  query me {
+    user(login: "MiyamotoKazuhiko") {
+      name
+      avatarUrl
+    }
+  }
+`
+export const SEARCH_REPOSITORIES = gql`
+	query searchRepositories ($first: Int, $after: String, $before: String, $last: Int, $query: String!){
+    search(first: $first, after: $after, before: $before, last: $last, query: $query, type: REPOSITORY) {
+      repositoryCount
+      pageInfo {
+        endCursor
+        hasNextPage
+        hasPreviousPage
+        startCursor
+      }
+      edges {
+        cursor
+        node {
+          ... on Repository {
+            id
+            name
+            url
+            stargazers{
+              totalCount
+            }
+            viewerHasStarred
+          }
+        }
+      }
+    }
+  }
+`
+export const ADD_STAR = gql`
+  mutation addStar ($input: AddStarInput!) {
+    addStar (input: $input) {
+      starrable {
+        id
+        viewerHasStarred
+      }
+    }
+  }
+`
+
+export const REMOVE_STAR = gql`
+  mutation removeStar ($input: RemoveStarInput!) {
+    removeStar (input: $input) {
+      starrable {
+        id
+        viewerHasStarred
+      }
+    }
+  }
+`
